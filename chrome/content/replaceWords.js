5bd06eb2cae24dc06429b4bdca4d41376b2d96df
brahmnoor <bsc73@drexel.edu>
2020-02-22 14:14:11 -0500

    first commit

---
+++ chrome/content/replaceWords.js
@@ -0,0 +1,11 @@
+console.log("LOG: Running replaceWords.js");
+
+const allText = $('body').text();
+
+const apiURL = ""
+
+const data = {
+  "text" : allText,
+  "language" : "spanish",
+  "currentLearningProgress" : cLearningProgress
+}
