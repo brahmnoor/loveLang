c04d3bb260c4e8a111cec733ca5df8509045baff
ac3965 <ac3965@cs.drexel.edu>
2020-02-23 10:49:12 -0500

    PS ansh is nitpicky

--- chrome/content/pop_display.js
+++ chrome/content/pop_display.js
@@ -9,7 +9,7 @@ function save_options() {
   }, function() {
     // Update status to let user know options were saved.
     var status = document.getElementById('status');
-    status.textContent = 'Options saved.';
+    status.textContent = 'Preference Saved.';
     setTimeout(function() {
       status.textContent = '';
     }, 4000);
