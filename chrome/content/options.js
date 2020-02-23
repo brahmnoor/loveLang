63832a154a18f89ea44cadcf64099cca3de39c44
sp3573 <sp3573@drexel.edu>
2020-02-23 10:40:14 -0500

    options made visible

---
+++ chrome/content/options.js
@@ -0,0 +1,29 @@
+// Saves options to chrome.storage
+// Saves options to chrome.storage
+function save_options() {
+  var language = document.getElementById('Languages').value;
+  chrome.storage.sync.set({
+    chosenLang: language,
+  }, function() {
+    // Update status to let user know options were saved.
+    var status = document.getElementById('status');
+    status.textContent = 'Options saved.';
+    setTimeout(function() {
+      status.textContent = '';
+    }, 5000);
+  });
+}
+
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+function restore_options() {
+  // Use default value color = 'red' and likesColor = true.
+  chrome.storage.sync.get({
+    chosenLang: 'English',
+  }, function(items) {
+    document.getElementById('Language').value = items.chosenLang;
+  });
+}
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click',
+    save_options);
--- chrome/content/pop_display.js
+++ chrome/content/pop_display.js
@@ -12,7 +12,7 @@ function save_options() {
     status.textContent = 'Options saved.';
     setTimeout(function() {
       status.textContent = '';
-    }, 750);
+    }, 4000);
   });
 
 }
