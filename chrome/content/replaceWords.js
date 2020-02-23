4ea2680650262b95395191dfbb1a2aad9ac640aa
ac3965 <ac3965@cs.drexel.edu>
2020-02-22 19:02:05 -0500

    Ansh: made edits to replaceWords.js added functions of code

--- chrome/content/replaceWords.js
+++ chrome/content/replaceWords.js
@@ -1,13 +1,36 @@
 console.log("LOG: Running replaceWords.js");
 
-var res = $('body  *').contents().map(function () {
-    if (this.nodeType == 3 && this.nodeValue.trim() != "") //check for nodetype==3 which is text and ignore empty text nodes
-        return this.nodeValue.trim().split(/\W+/);  //split the nodevalue to get words.
-}).get(); //get the array of words.
+function makeArray(){
+  var res = $('body  *').contents().map(function () {
+      if (this.nodeType == 3 && this.nodeValue.trim() != "") //check for nodetype==3 which is text and ignore empty text nodes
+          return this.nodeValue.trim().split(/\W+/);  //split the nodevalue to get words.
+  }).get(); //get the array of words.
 
-let uniq_out = [...new Set(res)];
-console.log(uniq_out);
+  let uniq_out = [...new Set(res)]; //deletes duplicate elements
+  return uniq_out;
+}
 
+function replaceWord(initialWord, finalWord){
 
-// var all = document.body.innerText;
-// $('body').text();
+    if (document.readyState === "complete") {
+      $("body").children().each(function () {
+      $(this).html( $(this).html().replace(/initialWord/gi,finalWord) );
+      });
+    } else {
+        setTimeout('checkLoad();', 500)
+    }
+}
+
+function translateWord(originalWord, languageCode) {
+  var url = "https://translation.googleapis.com/language/translate/v2";
+  //Strings requiring translation
+  url += "?q=" + originalWord;
+  url += "&target=" + languageCode;
+  //Replace with your API key
+  url += "&key=AIzaSyBPsptbtoOIk6GVvZZvtn2P6mbDzHp9ZoE";
+  $.get(url, function (data, status) {
+      //Results are returned in an array following the order they were passed.
+      this.finalTranslation = data.data.translations[0].translatedText;
+  });
+  return finalTranslation;
+}
