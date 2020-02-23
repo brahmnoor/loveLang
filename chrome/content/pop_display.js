8b28b96c2437fa533dbef92592ac2cda3dae56ad
brahmnoor <bsc73@drexel.edu>
2020-02-23 04:40:39 -0500

    got coffee for the fam and sang pichle saat dino mein

--- chrome/content/pop_display.js
+++ chrome/content/pop_display.js
@@ -1,1 +1,17 @@
-var 
+// Saves options to chrome.storage
+function save_options() {
+  var e = document.getElementById("Languages");
+  var selectedLanguage = e.options[e.selectedIndex].value;
+
+  chrome.storage.sync.set({
+    language: selectedLanguage,
+    masteredWords: []
+  }, function() {
+    // Update status to let user know options were saved.
+    var status = document.getElementById('status');
+    status.textContent = 'Options saved.';
+    setTimeout(function() {
+      status.textContent = '';
+    }, 750);
+  });
+}
