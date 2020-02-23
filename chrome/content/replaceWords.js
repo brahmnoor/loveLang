26c611db920f822a6505bb03f31ce5e3de3bb2cf
brahmnoor <bsc73@drexel.edu>
2020-02-22 23:17:41 -0500

    did some modifications to frontend

--- chrome/content/replaceWords.js
+++ chrome/content/replaceWords.js
@@ -9,7 +9,9 @@ console.log("LOG: Running replaceWords.js");
 //   wrapClass: 'Shiny'
 // });
 
-function makeArray(){
+// console.log(makeArrayAndReturnObject());
+
+function makeArrayAndReturnObject() {
   var res = $('body  *').contents().map(function () {
       if (this.nodeType == 3 && this.nodeValue.trim() != "") //check for nodetype==3 which is text and ignore empty text nodes
           return this.nodeValue.trim().split(/\W+/);  //split the nodevalue to get words.
@@ -17,8 +19,19 @@ function makeArray(){
   for (i = 0; i < res.length; i++){
     res[i] = res[i].toLowerCase();
   }
-  let uniq_out = [...new Set(res)]; //deletes duplicate elements
-  return uniq_out;
+  var wordObj = _.countBy(res);
+  var sortable = [];
+  for (var word in wordObj) {
+      sortable.push([word, wordObj[word]]);
+  }
+
+  sortable.sort(function(a, b) {
+      return b[1] - a[1];
+  });
+
+  return sortable;
+  // let uniq_out = [...new Set(res)]; //deletes duplicate elements
+  // return uniq_out;
 }
 
 function replaceWord(initialWord, newWord){
